fa1c5782e2037d6877b5a47b38128529e6125e3a
Yohan Ferreira <yohan.dev@outlook.fr>
2023-03-20 10:32:46 +0100

    update NewBills -> compatibility file upload

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -17,9 +17,19 @@ export default class NewBill {
   }
   handleChangeFile = e => {
     e.preventDefault()
+    const input = this.document.querySelector(`input[data-testid="file"]`)
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
+    const fileName = filePath[filePath.length - 1]
+
+    //  Empêche l'utilisateur d'utiliser un fichier autre qu'un format png, jpeg, jpg et l'averti via une alerte.
+    if (!/.(?:png|jpeg|jpg)/.test(file.name)) {
+      this.document.getElementById("btn-send-bill").disabled = true;
+      alert('Only jpeg, jpg or png format are suported.')
+      return (1)
+    }
+
+    this.document.getElementById("btn-send-bill").disabled = false
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
     formData.append('file', file)
@@ -33,7 +43,7 @@ export default class NewBill {
           noContentType: true
         }
       })
-      .then(({fileUrl, key}) => {
+      .then(({ fileUrl, key }) => {
         console.log(fileUrl)
         this.billId = key
         this.fileUrl = fileUrl
@@ -47,9 +57,9 @@ export default class NewBill {
     const bill = {
       email,
       type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
+      name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
       amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
-      date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
+      date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
       vat: e.target.querySelector(`input[data-testid="vat"]`).value,
       pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
       commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
@@ -65,12 +75,12 @@ export default class NewBill {
   updateBill = (bill) => {
     if (this.store) {
       this.store
-      .bills()
-      .update({data: JSON.stringify(bill), selector: this.billId})
-      .then(() => {
-        this.onNavigate(ROUTES_PATH['Bills'])
-      })
-      .catch(error => console.error(error))
+        .bills()
+        .update({ data: JSON.stringify(bill), selector: this.billId })
+        .then(() => {
+          this.onNavigate(ROUTES_PATH['Bills'])
+        })
+        .catch(error => console.error(error))
     }
   }
 }
